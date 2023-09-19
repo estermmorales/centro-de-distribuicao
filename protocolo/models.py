d3dee15ddec332889e525e7d315001c5c9bbecd2
Ester Morales <estermorales.business@gmail.com>
2023-09-18 23:20:56 -0300

    criando models

--- protocolo/models.py
+++ protocolo/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Permissao(models.Model):
+    permissoes = ["Gerente", "Funcionário", "Estagiário", "Visitante"]
+    descricao = models.CharField(max_length=45, choices=permissoes)
+
+
+class Perfil(models.Model):
+    descricao = models.CharField(max_length=45)
+    id_perimssao = models.ForeignKey(Permissao)
+
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=45)
+    senha = models.CharField(max_length=30)
+    id_perfil = models.ForeignKey(Perfil)
+
+
+class EmitenteDestinatario(models.Model):
+    nome = models.CharField(max_length=45)
+    documento = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+    email = models.CharField(max_length=45)
+
+
+class Situacao(models.Model):
+    situacoes = ["PENDENTE", "RETIRADO", "CANCELADO"]
+    descricao = models.CharField(max_length=45, choices=situacoes)
+
+
+class Protocolo(models.Model):
+    data_entrega = models.DateField()
+    data_retirada = models.DateField()
+    id_emitente = models.ForeignKey(EmitenteDestinatario)
+    id_destinatario = models.ForeignKey(EmitenteDestinatario)
+    id_usuario = models.ForeignKey(Usuario)
+    id_situacao = models.ForeignKey(Situacao)
+    qtd_volumes = models.IntegerField(max_length=11)
