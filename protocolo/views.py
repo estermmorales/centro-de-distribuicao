58c7479ec6f71cbd5603a1d3c61339bd38400ac4
Ester Morales <estermorales.business@gmail.com>
2023-11-01 14:26:38 -0300

    testre

--- protocolo/views.py
+++ protocolo/views.py
@@ -6,7 +6,7 @@ from .models import Funcionario, Protocolo
 from .forms import ProtocoloForm, ProtocoloEditForm
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 from django.views.generic import ListView
 
 def protocolo(request):
