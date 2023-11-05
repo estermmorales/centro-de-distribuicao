54995a97fa5868d03a3880979afe72ff61fdd03e
brunabocalon <149969578+brunabocalon@users.noreply.github.com>
2023-11-05 18:52:54 -0300

    Add files via upload
    foram criados mais dois arquivos html para as paginas de erro, a estilização foi adicionada no card.scss, foram criadas urls e views para as novas telas

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.protocolo, name='protocolo'),
+    path('cadastrar_protocolo/', views.cadastrar_protocolo, name='cadastrar_protocolo'),
+    path('protocolos/editar/', views.editar_protocolo, name='editar_protocolo'),
+    path('autocomplete_usuarios/', views.autocomplete_usuarios, name='autocomplete_usuarios'),
+    path('usuarios', views.usuarios, name='usuarios'),
+    path('cadastrar_usuario/', views.cadastrar_usuarios, name='cadastrar_usuario'),
+    path('usuarios/editar/', views.editar_usuario, name='editar_usuario'),
+    path('usuarios/autocomplete_usuarios/', views.autocomplete_usuarios, name='autocomplete_usuarios'),
+    path('historico', views.historico, name='historico'),
+    path('404', views.erro, name='erro'),
+    path('500', views.erro500, name='erro500'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
