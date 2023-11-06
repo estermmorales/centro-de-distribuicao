8e9c0ed1611a1c275024676dcdad2a24fdfefef1
brunabocalon <brunabocalon08@gmail.com>
2023-11-06 19:54:26 -0300

    alteracao

--- protocolo/urls.py
+++ protocolo/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('cadastrar_usuario/', views.cadastrar_usuarios, name='cadastrar_usuario'),
     path('usuarios/editar/', views.editar_usuario, name='editar_usuario'),
     path('usuarios/autocomplete_usuarios/', views.autocomplete_usuarios, name='autocomplete_usuarios'),
+    path('historico', views.historico, name='historico'),
     path('historico', views.historico, name='historico')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
